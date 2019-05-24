6e58ab60eac4a5dda9ab78fecf7b058f06757727
daniel-wtd <daniel@while-true-do.io>
2019-05-24 20:27:03 +0200

    fix: testing

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -17,17 +17,3 @@ def test_audit_service(host):
         srv = host.service('auditd')
         assert srv.is_running
         assert srv.is_enabled
-
-
-def test_audit_conf(host):
-    file = host.file('/etc/audit/audit.conf')
-    assert file.is_file
-    assert file.user == 'root'
-    assert file.group == 'root'
-
-
-def test_audit_rules(host):
-    file = host.file('/etc/audit/rules.d/additional.rules')
-    assert file.is_file
-    assert file.user == 'root'
-    assert file.group == 'root'
